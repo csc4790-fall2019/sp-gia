ae9b1b9ceaed5033689f4c242e0a294b0e6b1ccf
gabbywenn28 <45183047+gabbywenn28@users.noreply.github.com>
2019-11-17 12:11:48 -0500

    Messaging components if we have time.

--- DormMate/src/app/app.module.ts
+++ DormMate/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 //import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
@@ -17,10 +20,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule//,
+    FormsModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbChatModule//,
     //FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+export class PageModule { }
 export class AppModule { }
