38740f05cce80b605ffa3205bed46ca6c244907d
gabbywenn28 <45183047+gabbywenn28@users.noreply.github.com>
2019-12-11 21:23:34 -0500

    Possible fixes 13

--- DormMate/src/app/component/profile/profile.component.ts
+++ DormMate/src/app/component/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { UserdataService } from '../../userdata.service';
 import { AuthService, FacebookLoginProvider, SocialUser } from 'angularx-social-login';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +11,39 @@ import { AuthService, FacebookLoginProvider, SocialUser } from 'angularx-social-
 })
 export class ProfileComponent implements OnInit {
 
+  usersList: any = [];
+  updateUser: FormGroup;
+
   public user: SocialUser;
   public loggedIn: boolean;
 
-  constructor(private authService: AuthService, private userdataService: UserdataService) { }
+  constructor(private authService: AuthService,
+    private userdataService: UserdataService,
+    public fb: FormBuilder,
+    private ngZone: NgZone,
+    private router: Router,
+    private actRoute: ActivatedRoute,
+  ) {
+    var id = this.actRoute.snapshot.paramMap.get('id');
+    this.userdataService.getUser(id).subscribe((data) => {
+      this.updateUser = this.fb.group({
+        firstName: [data.firstName],
+        lastName: [data.lastName],
+        Class: [data.Class],
+        Hometown: [data.Hometown],
+        Major: [data.Major],
+        Bedtime: [data.Bedtime],
+        Bio: [data.Bio],
+        Gender: [data.Gender],
+        Smoker: [data.Smoker],
+        Alcohol: [data.Alcohol],
+        Neatness: [data.Neatness],
+        Snore: [data.Snore]
+      })
+    });
+   }
+
+
 
   signInWithFB(): void {
     this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
